3ea0d6b15939cb17d53495e5b2024a7d6e6bad2a
nopparet <tnopparet990@gmail.com>
2024-07-31 11:17:31 +0700

    monified reset() methot

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -96,6 +96,8 @@ export default {
       },
 
       onReset () {
+        id.value = null
+        surname.value = null
         name.value = null
         age.value = null
         accept.value = false
